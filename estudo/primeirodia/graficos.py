598cdd7dc2e2ac4f27e248fdce8375f9c1c7b571
João Lucas de Lima Serafim <168292874+Serafinoo@users.noreply.github.com>
2026-02-27 16:39:40 -0300

    making better graphics

--- estudo/primeirodia/graficos.py
+++ estudo/primeirodia/graficos.py
@@ -14,11 +14,39 @@ comidas = pd.DataFrame({
     "FR": [0.715, 0.165, 0, 0.11, 0, 0, 0, 0.055, 1.0]
 })
 
-fig = px.bar(dados, x="Frequência", y="FR", text="FA", title="Frequência de consumo comidas típicas: 2º EM - Vespertino")
-fig.update_layout(yaxis_tickformat=".0%")
-fig.update_traces(textposition="outside")
+fig = make_subplots(
+    rows=1, cols=2,
+    specs=[[{"type": "bar"}, {"type": "pie"}]],
+    subplot_titles=("Frequência de consumo comidas típicas: 2º EM - Vespertino", "Comidas típicas favoritas: 2º EM - Vespertino")
+)
+
+fig.add_trace(
+    go.Bar(
+        x=dados["Frequência"],
+        y=dados["FR"]
+        text=dados["FA"]
+        textposition="outside"
+        fig.update_yaxes(tickformat=".0%", row=1, col=1)
+    ),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Pie(
+        labels=comidas["Exemplares"],
+        values=comidas["FA"]
+    ),
+    row=1,col=2
+)
+
+fig.update_layout(title="Comidas Típicas: 2º EM - Vespertino")
 fig.show()
 
-fig2 = px.pie(comidas, names="Exemplares", values="FA",title="Comidas típicas favoritas: 2º EM - Vespertino")
-fig2.update_layout(yaxis_tickformat=".0%")
-fig2.show()
+#    fig = px.bar(dados, x="Frequência", y="FR", text="FA", title="Frequência de consumo comidas típicas: 2º EM - Vespertino")
+#    fig.update_layout(yaxis_tickformat=".0%")
+#    fig.update_traces(textposition="outside")
+#    fig.show()
+#
+#    fig2 = px.pie(comidas, names="Exemplares", values="FA",title="Comidas típicas favoritas: 2º EM - Vespertino")
+#    fig2.update_layout(yaxis_tickformat=".0%")
+#    fig2.show()
